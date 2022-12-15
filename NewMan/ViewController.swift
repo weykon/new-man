7aea0d59c43f3ff8c74bb2a94f3da7c19bbdcae8
weykon <635883092@qq.com>
2022-12-15 15:38:07 +0800

    Initial Commit

---
+++ NewMan/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  NewMan
+//
+//  Created by Weykon Kong on 2022/12/15.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
